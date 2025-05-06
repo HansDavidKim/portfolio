f70911ce596bb9cfb04f77f776b37a15a3a3ed01
Hans <eodnjs5498@naver.com>
2025-05-07 00:41:33 +0900

    Study Sample

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,47 +1,25 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+    let name = 'World';
+    $: fullName = 'John ' + name;
+    import {count} from './store.js';
+    let testArray = [1, 2, 3];
 </script>
-
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+    <h1>Hello, {name}!</h1>
+    <button on:click={() => 
+    {
+        if (name === 'World') {
+            name = 'Svelte';
+        } else {
+            name = 'World';
+        }
+    }
+    }>Change Name</button>
+    <h2>Full Name: {fullName}</h2>
+    {#each testArray as item}
+        <p>{item}</p>
+    {/each}
+    <h2>Count: {$count}</h2>
+    <button on:click={() => count.update(n => n + 1)}>Increment</button>
+    <button on:click={() => count.update(n => n - 1)}>Decrement</button>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
